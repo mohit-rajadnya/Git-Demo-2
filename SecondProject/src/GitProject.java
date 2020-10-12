d93de4c8c616f775086ffdb340f3844f1494e098
mohit-rajadnya <72739396+mohit-rajadnya@users.noreply.github.com>
2020-10-12 17:32:22 +0530

    Update GitProject.java

--- SecondProject/src/GitProject.java
+++ SecondProject/src/GitProject.java
@@ -3,6 +3,8 @@ public class GitProject {
 
 	public static void main(String[] args) {
 		System.out.println("Trial of second github repo");
+		
+		System.out.println("Changes made on github");
 
 	}
 
